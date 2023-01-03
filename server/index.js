77b597a43e7e596c585654893a7962a403226a1c
Akash RP <akashrp512@gmail.com>
2023-01-03 21:25:59 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -9,9 +9,13 @@ import morgan from "morgan";
 import Path from "path";
 import { fileURLToPath } from "url";
 import path from "path";
-import {register} from ",/controllers/auth.js";
+import {register} from "./controllers/auth.js";
+import {createPost} from "./controllers/posts.js"
 import authRoutes from "./routes/auth.js";
-import userRoutes from  "./routes/users.js"
+import userRoutes from  "./routes/users.js";
+import postRoutes from "./routes/posts.js"
+
+import { verifyToken } from "./middleware/auth.js";
 
 
 /* CONFIGURATION */
@@ -42,10 +46,12 @@ const upload = multer({storage});
 
 /* ROUTES WITH FILES*/
 app.post("auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost )
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts",  postRoutes)
 /* MONGOOSE SETUP*/
 
 const PORT = process.env.PORT || 6001;
