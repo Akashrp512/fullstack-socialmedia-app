478ab4ad2ddfb33fa9e37270c2ad02e782122927
Akash RP <akashrp512@gmail.com>
2023-01-04 10:43:18 +0530

    Backend

--- server/index.js
+++ server/index.js
@@ -14,8 +14,10 @@ import {createPost} from "./controllers/posts.js"
 import authRoutes from "./routes/auth.js";
 import userRoutes from  "./routes/users.js";
 import postRoutes from "./routes/posts.js"
-
 import { verifyToken } from "./middleware/auth.js";
+import  User from "./models/User.js";
+import Post from "./models/post.js";
+import {users, posts} from "./data/index.js"
 
 
 /* CONFIGURATION */
@@ -61,6 +63,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(()=>{
     app.listen(PORT, ()=>console.log(`Server Port: ${PORT}`));
+// /* ADD DATA ONE TIME */
+//     User.insertMany(users);
+//     Post.insertMany(posts);
 }).catch((error)=>console.log(`${error} did not connect`));
 
 
